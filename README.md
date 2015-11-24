43dbd6c18ea505650859102fc089a4aadf9fbdf9
Jimmy <jgs2017@gmail.com>
2015-11-24 12:42:56 -0600

    add test markers

---
+++ README.md
@@ -0,0 +1,12 @@
+#Kansas City Map
+
+This is a map project I'm currently working as part of the Udacity Front-End Web Developer Nanodegree.  Please visit Udacity.com to learn more about their program.
+
+https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001
+
+The project is work-in-progress.
+
+## How to Use
+
+1. Clone the repo with your terminal: `git clone git@github.com:Yojim6o/KCmap.git`
+2. Open the folder KCmap/ and open index.html in the browser of your choice.
--- js/main.js
+++ js/main.js
@@ -1,7 +1,32 @@
+'use strict';
+
 var map;
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 39.094681, lng: -94.582033},
         zoom: 12
     });
+
+    function point(name, lat, long) {
+        this.name = name;
+        this.lat = ko.observable(lat);
+        this.long = ko.observable(long);
+
+        var marker = new google.maps.Marker({
+            position: new google.maps.LatLng(lat, long),
+            title: name,
+            map: map
+            // draggable: true
+        });
+    }
+
+    var viewModel = {
+        points: ko.observableArray([
+            new point('Test1', 39.132073, -94.505719),
+            new point('Test2', 39.174671, -94.577130),
+            new point('Test3', 39.079857, -94.570263)
+        ])
+    };
+
+    ko.applyBindings(viewModel);
 }
