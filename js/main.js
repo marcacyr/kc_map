0a9a1adc0072ca2e3973052672520f93656b3a7c
Jimmy <jgs2017@gmail.com>
2015-12-03 00:21:22 -0600

    add <ul> filter

--- js/main.js
+++ js/main.js
@@ -2,33 +2,77 @@
 
 var map;
 function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 39.076268, lng: -94.590043},
-        zoom: 13
+  map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: 39.076268, lng: -94.590043},
+      zoom: 13
+  });
+
+  function point(name, lat, long) {
+    this.name = name;
+    this.lat = ko.observable(lat);
+    this.long = ko.observable(long);
+
+    var marker = new google.maps.Marker({
+      position: new google.maps.LatLng(lat, long),
+      title: name,
+      map: map
     });
 
-    function point(name, lat, long) {
-        this.name = name;
-        this.lat = ko.observable(lat);
-        this.long = ko.observable(long);
+    this.isVisible = ko.observable(false);
 
-        var marker = new google.maps.Marker({
-            position: new google.maps.LatLng(lat, long),
-            title: name,
-            map: map
-            // draggable: true
-        });
-    }
+    this.isVisible.subscribe(function(currentState) {
+      if (currentState) {
+        marker.setMap(map);
+      } else {
+        marker.setMap(null);
+      }
+    });
 
-    var viewModel = {
-        points: ko.observableArray([
-            new point('801 Chop House', 39.096980, -94.582418),
-            new point('Grinders', 39.091405, -94.578115),
-            new point('Jack Stack Barbecue', 39.087237, -94.585817),
-            new point('Beer Kitchen', 39.052808, -94.591287),
-            new point('Reserve', 39.100631, -94.580513)
-        ])
-    };
+    this.isVisible(true);
+  }
 
-    ko.applyBindings(viewModel);
+  var viewModel = {
+    points: ko.observableArray([
+      new point('801 Chop House', 39.096980, -94.582418),
+      new point('Grinders', 39.091405, -94.578115),
+      new point('Jack Stack Barbecue', 39.087237, -94.585817),
+      new point('Beer Kitchen', 39.052808, -94.591287),
+      new point('Reserve', 39.100631, -94.580513)
+    ]),
+
+  };
+
+  ko.applyBindings(viewModel);
 }
+
+///////////////////
+
+var places = [
+  {name:"801 Chop House"},
+  {name:"Grinders"},
+  {name:"Jack Stack Barbecue"},
+  {name:"Beer Kitchen"},
+  {name:"Reserve"},
+];
+
+$(function() {
+
+  var viewModel = {
+    places: ko.observableArray(places),
+
+    query: ko.observable(''),
+
+    search: function(value) {
+      viewModel.places([]);
+      for(var x in places) {
+        if(places[x].name.toLowerCase().indexOf(value.toLowerCase()) >= 0) {
+          viewModel.places.push(places[x]);
+        }
+      }
+    }
+  };
+
+  viewModel.query.subscribe(viewModel.search);
+
+  ko.applyBindings(viewModel);
+});
