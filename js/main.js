1d94b3f75c86a28dab1812fa3fed78e59e69fff0
Jimmy <jgs2017@gmail.com>
2015-12-03 09:47:43 -0600

    isolate data object

--- js/main.js
+++ js/main.js
@@ -7,20 +7,25 @@ function initMap() {
       zoom: 13
   });
 
-  function point(name, lat, long) {
-    this.name = name;
-    this.lat = ko.observable(lat);
-    this.long = ko.observable(long);
+  //maybe a for loop that doesn't create new points, but instead just
+  //creates markers for each object in the model
+
+  for (var i = 0; i < places.length; i++) {
+    // places[i].name = name;
+    // places[i].lat = ko.observable(lat);
+    // places[i].long = ko.observable(long);
 
     var marker = new google.maps.Marker({
-      position: new google.maps.LatLng(lat, long),
-      title: name,
+      position: new google.maps.LatLng(places[i].lat, places[i].long),
+      title: places[i].name,
       map: map
     });
 
-    this.isVisible = ko.observable(false);
+    // marker.setMap(map);
 
-    this.isVisible.subscribe(function(currentState) {
+    places[i].isVisible = ko.observable(false);
+
+    places[i].isVisible.subscribe(function(currentState) {
       if (currentState) {
         marker.setMap(map);
       } else {
@@ -28,31 +33,16 @@ function initMap() {
       }
     });
 
-    this.isVisible(true);
-  }
-
-  var viewModel = {
-    points: ko.observableArray([
-      new point('801 Chop House', 39.096980, -94.582418),
-      new point('Grinders', 39.091405, -94.578115),
-      new point('Jack Stack Barbecue', 39.087237, -94.585817),
-      new point('Beer Kitchen', 39.052808, -94.591287),
-      new point('Reserve', 39.100631, -94.580513)
-    ]),
-
+    places[i].isVisible(true);
   };
-
-  ko.applyBindings(viewModel);
 }
 
-///////////////////
-
 var places = [
-  {name:"801 Chop House"},
-  {name:"Grinders"},
-  {name:"Jack Stack Barbecue"},
-  {name:"Beer Kitchen"},
-  {name:"Reserve"},
+  {name:"801 Chop House", lat: 39.096980, long: -94.582418},
+  {name:"Grinders", lat: 39.091405, long: -94.578115},
+  {name:"Jack Stack Barbecue", lat: 39.087237, long: -94.585817},
+  {name:"Beer Kitchen", lat: 39.052808, long: -94.591287},
+  {name:"Reserve", lat: 39.100631, long: -94.580513}
 ];
 
 $(function() {
