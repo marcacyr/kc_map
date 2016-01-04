ba91e0dd01852feaf052179390ae3bb4e5e0bcb3
Jimmy <jgs2017@gmail.com>
2016-01-04 13:58:58 -0600

    get map markers to listen

---
+++ js/model.js
@@ -0,0 +1,32 @@
+var places = [
+{
+	name: "801 Chop House",
+	lat: 39.096980,
+	long: -94.582418,
+	visible: ko.observable(true),
+	id: "spot1"
+}, {
+	name: "Grinders",
+	lat: 39.091405,
+	long: -94.578115,
+	visible: ko.observable(true),
+	id: "spot2"
+}, {
+	name: "Jack Stack Barbecue",
+	lat: 39.087237,
+	long: -94.585817,
+	visible: ko.observable(true),
+	id: "spot3"
+}, {
+	name: "Beer Kitchen",
+	lat: 39.052808,
+	long: -94.591287,
+	visible: ko.observable(true),
+	id: "spot4"
+}, {
+	name: "Reserve",
+	lat: 39.100631,
+	long: -94.580513,
+	visible: ko.observable(true),
+	id: "spot5"
+}]
--- js/viewmodel.js
+++ js/viewmodel.js
@@ -1,16 +1,19 @@
 'use strict';
 
+//set map variable
 var map;
+
+//define map, center and zoom to KC, and call initMarkers
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
       center: {lat: 39.076268, lng: -94.590043},
       zoom: 13
   });
-
-  setMarkers(places);
+  initMarkers(places);
 }
 
-function setMarkers(places) {
+//create markers for each place in the model
+function initMarkers(places) {
   for (var i = 0; i < places.length; i++) {
     places[i].holdMarker = new google.maps.Marker({
       position: new google.maps.LatLng(places[i].lat, places[i].long),
@@ -20,9 +23,10 @@ function setMarkers(places) {
   };
 }
 
-function setAllMap() {
+//update the visibility of each marker based on search query
+function updateMarkers() {
   for (var i=0; i < places.length; i++) {
-    if (places[i].boolTest === true) {
+    if (places[i].live === true) {
       places[i].holdMarker.setMap(map);
     }
     else {
@@ -31,29 +35,32 @@ function setAllMap() {
   }
 }
 
+//the viewmodel object with search bar input set to no value.
 var viewModel = {
   query: ko.observable(''),
 };
 
-viewModel.places = ko.dependentObservable(function() {
+//set the model to computed observable and immediately invoke
+viewModel.places = ko.pureComputed(function() {
   var self = this;
   var search = self.query().toLowerCase();
   return ko.utils.arrayFilter(places, function(place) {
     if (place.name.toLowerCase().indexOf(search) >=0) {
-      place.boolTest = true;
+      place.live = true;
       return place.visible(true);
     }
     else {
-      place.boolTest = false;
-      setAllMap();
+      place.live = false;
+      updateMarkers();
       return place.visible(false);
     }
   });
 }, viewModel);
 
+//ready aim fire KO
 ko.applyBindings(viewModel);
 
+//bind a keyup function to the search bar that invokes the function updateMarkers
 $("#input").keyup(function() {
-  setAllMap();
+  updateMarkers();
 });
-
