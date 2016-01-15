98ac2c06bb823f8ead112a8dc48889263c03415d
Jimmy <jgs2017@gmail.com>
2016-01-15 15:36:29 -0600

    feat: google places api

--- js/model.js
+++ js/model.js
@@ -4,29 +4,34 @@ var places = [
 	lat: 39.096980,
 	long: -94.582418,
 	visible: ko.observable(true),
-	id: "spot0"
+	id: "spot0",
+	place_id: "ChIJfy9J7WfwwIcR4mkyLBq-GDk"
 }, {
 	name: "Grinders",
 	lat: 39.091405,
 	long: -94.578115,
 	visible: ko.observable(true),
-	id: "spot1"
+	id: "spot1",
+	place_id: "ChIJ29SyTWnwwIcRYYYjzNhRpF4"
 }, {
 	name: "Jack Stack Barbecue",
 	lat: 39.087237,
 	long: -94.585817,
 	visible: ko.observable(true),
-	id: "spot2"
+	id: "spot2",
+	place_id: "ChIJmzokTUDwwIcRwp_X-3ZvQMw"
 }, {
 	name: "Beer Kitchen",
 	lat: 39.052808,
 	long: -94.591287,
 	visible: ko.observable(true),
-	id: "spot3"
+	id: "spot3",
+	place_id: "ChIJ5YhmJMXvwIcRqkhrXVcSee4"
 }, {
 	name: "Reserve",
 	lat: 39.100631,
 	long: -94.580513,
 	visible: ko.observable(true),
-	id: "spot4"
+	id: "spot4",
+	place_id: "ChIJESPgKWfwwIcRJnKcGX3vfRU"
 }]
--- js/viewmodel.js
+++ js/viewmodel.js
@@ -19,32 +19,49 @@ function initMap() {
 //create markers for each place in the model
 function initMarkers(places) {
   for (var i = 0; i < places.length; i++) {
-    places[i].holdMarker = new google.maps.Marker({
-      position: new google.maps.LatLng(places[i].lat, places[i].long),
-      title: places[i].name,
-      map: map
-    });
+    // places[i].holdMarker = new google.maps.Marker({
+    //   position: new google.maps.LatLng(places[i].lat, places[i].long),
+    //   title: places[i].name,
+    //   map: map
+    // });
 
     var infowindow = new google.maps.InfoWindow();
+    var service = new google.maps.places.PlacesService(map);
+
+    service.getDetails({
+      placeId: places[i].place_id
+    }, function(place, status) {
+      if (status === google.maps.places.PlacesServiceStatus.OK) {
+        var marker = new google.maps.Marker({
+          map: map,
+          position: place.geometry.location
+        });
+        google.maps.event.addListener(marker, 'click', function() {
+          infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
+            place.formatted_address + '</div>');
+          infowindow.open(map, this);
+        });
+      }
+    });
 
     //if marker is clicked, display its info window
-    google.maps.event.addListener(places[i].holdMarker, 'click', (function(place, i) {
-      return function () {
-        animateMarker(place, i);
-        infowindow.setContent(places[i].name);
-        infowindow.open(map,this);
-      };
-    })(places[i].holdMarker, i));
+    // google.maps.event.addListener(places[i].holdMarker, 'click', (function(place, i) {
+    //   return function () {
+    //     animateMarker(place, i);
+    //     infowindow.setContent(places[i].name);
+    //     infowindow.open(map,this);
+    //   };
+    // })(places[i].holdMarker, i));
 
-    //if nav tool is clicked, display info window above marker
-    var searchNav = $('#spot' + i);
-    searchNav.click((function(place, i) {
-      return function() {
-        animateMarker(place, i);
-        infowindow.setContent(places[i].name);
-        infowindow.open(map,place);
-      };
-    })(places[i].holdMarker, i));
+    // //if nav tool is clicked, display info window above marker
+    // var searchNav = $('#spot' + i);
+    // searchNav.click((function(place, i) {
+    //   return function() {
+    //     animateMarker(place, i);
+    //     infowindow.setContent(places[i].name);
+    //     infowindow.open(map,place);
+    //   };
+    // })(places[i].holdMarker, i));
 
   };
 }
@@ -100,9 +117,9 @@ $("#input").keyup(function() {
 
 //toggle the visibility of side bar when clicking hamburger icon
 $("#hamburger").click(function () {
-    if ( $('#sidebar').css('z-index') == 0) {
-        $('#sidebar').css('z-index', 10);
-    } else {
-        $('#sidebar').css('z-index', 0);
-    };
+  if ( $('#sidebar').css('z-index') == 0) {
+    $('#sidebar').css('z-index', 10);
+  } else {
+    $('#sidebar').css('z-index', 0);
+  };
 });
