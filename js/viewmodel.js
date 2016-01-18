de56c35199a6b55c7d17b5cf7cb3d16bfcc5a8be
Marc Cyr <marcacyr@gmail.com>
2016-01-17 20:54:29 -0800

    refactoring to try to make things easier

--- js/viewmodel.js
+++ js/viewmodel.js
@@ -13,7 +13,24 @@ function initMap() {
           position: google.maps.ControlPosition.BOTTOM_CENTER
       }
   });
-  initMarkers(places);
+  initMarkers.call(this, places);
+}
+
+function callback(place, status) {
+  var infowindow = window.infowindow;
+  var service = window.service;
+
+  if (status === google.maps.places.PlacesServiceStatus.OK) {
+    var marker = new google.maps.Marker({
+      map: map,
+      position: place.geometry.location
+    });
+    google.maps.event.addListener(marker, 'click', function() {
+      infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
+        place.formatted_address + '</div>');
+      infowindow.open(map, this);
+    });
+  }
 }
 
 //create markers for each place in the model
@@ -24,25 +41,12 @@ function initMarkers(places) {
     //   title: places[i].name,
     //   map: map
     // });
-
-    var infowindow = new google.maps.InfoWindow();
-    var service = new google.maps.places.PlacesService(map);
+    window.infowindow = new google.maps.InfoWindow();
+    window.service = new google.maps.places.PlacesService(map);
 
     service.getDetails({
       placeId: places[i].place_id
-    }, function(place, status) {
-      if (status === google.maps.places.PlacesServiceStatus.OK) {
-        var marker = new google.maps.Marker({
-          map: map,
-          position: place.geometry.location
-        });
-        google.maps.event.addListener(marker, 'click', function() {
-          infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
-            place.formatted_address + '</div>');
-          infowindow.open(map, this);
-        });
-      }
-    });
+    }, callback);
 
     //if marker is clicked, display its info window
     // google.maps.event.addListener(places[i].holdMarker, 'click', (function(place, i) {
